42e80254cfcff2c181effc61b6b9423b20daba69
Josh Wilkinson <joshlwilkinson9@gmail.com>
2022-11-27 20:18:20 +1100

    add cmd line arg parsing with argparse, remove pdf file

--- combine.py
+++ combine.py
@@ -1,13 +1,18 @@
 #! /usr/bin/python3
 import subprocess
 import sys
+import argparse
 
-if (len(sys.argv) != 3):
-    print("Usage: python3 combine.py input.pdf output")
-    sys.exit()
-file = sys.argv[1]
-new_name = sys.argv[2]
+parser = argparse.ArgumentParser(description='Generate a note-taking pdf from a slide deck')
+parser.add_argument("-i", "--input", help="input file", required=True)
+parser.add_argument("-o", "--output", help="output file", required=True)
 
+args = parser.parse_args()
+file = args.input
+new_name = args.output
+
+# todo im adding a comment from the github ios app
+# AND SOME MORE STUFF HERE
 cmd = "rm sep/*"
 print(subprocess.run(cmd, shell=True))
 
@@ -28,5 +33,5 @@ subprocess.run(cmd.split())
 
 cmd = f"pdflatex --output-directory=out combine.tex"
 subprocess.run(cmd.split())
-cmd = f"gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -sOutputFile={new_name}.pdf out/combine.pdf"
+cmd = f"gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -sOutputFile={new_name} out/combine.pdf"
 subprocess.run(cmd.split())
